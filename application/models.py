0f909c6e38e738012cd064d57ebea3680c263d5c
James C <JCarr@qa.com>
2021-05-11 10:05:53 +0000

    Finished table schema for armies and units

--- application/models.py
+++ application/models.py
@@ -1,6 +1,16 @@
 from application import db
 
-class Amry(db.Model):
+class Army(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(50), )
-    complete = db.Column(db.Boolean, default = False)
+    name = db.Column(db.String(50), nullable = False, unique = True)
+    Faction = db.Column(db.String(50), nullable = False)
+    Codex = db.Column(db.Integer, nullable = False)
+    units = db.relationship('Unit', backref='Army')
+
+class Unit(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50), nullable = False, unique = True)
+    category = db.Column(db.String(50), nullable = False)
+    price = db.Column(db.Integer, nullable = False)
+    quantity = db.Column(db.Integer, nullable = False)
+    army_id = db.Column(db.Integer, db.ForeignKey('army.id'), nullable=False)

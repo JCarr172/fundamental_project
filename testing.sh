ab4abc2b8ca691dc4fc52b8f3ac8b2b01762adb3
James Carr <82572701+JCarr172@users.noreply.github.com>
2021-05-21 14:16:31 +0100

    Create testing.sh

---
+++ testing.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#apt install
+sudo apt-get update
+sudo apt-get install python3 python3-venv python3-pip -y
+
+#setup venv and install pip requirements
+python3 -m venv venv
+source venv/bin/activate
+pip3 install -r requirements.txt
+
+
+python3 -m pytest --cov=application
